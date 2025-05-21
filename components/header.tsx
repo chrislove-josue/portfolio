38eb735b5255b3db26aa32478054899b52cb7c57
chrislove-nestor <sounesatchougo@gmail.com>
2025-05-21 14:56:50 +0100

    mise à jour header

--- components/header.tsx
+++ components/header.tsx
@@ -73,7 +73,7 @@ export function Header() {
 
         {isMobile ? (
           <>
-            <div className="flex items-center gap-3">
+            <div className="flex items-center gap-2">
               <LanguageSelectorDropdown />
               <div onClick={(e) => e.stopPropagation()}>
                 <ModeToggle />
