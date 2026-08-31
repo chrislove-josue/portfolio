0d4b6d94f2a422c8b76983da6adf0ba4eb032f2f
chrislove-josue <chrislovenestor@gmail.com>
2026-08-31 18:17:39 +0200

    npm i @vercel/speed-insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import { Inter } from "next/font/google"
 import { LanguageProvider } from "@/contexts/language-context"
 import { ThemeProvider } from "next-themes"
 import { Analytics } from "@vercel/analytics/next"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -84,6 +85,7 @@ export default function RootLayout({
           <LanguageProvider>{children}</LanguageProvider>
         </ThemeProvider>
         <Analytics/>
+        <SpeedInsights/>
       </body>
     </html>
   )
